ef5e356ad609a3e1d74f45fb683c644e79db31ef
Nathan Lenogue <nathan.lenogue@epita.fr>
2022-03-15 01:25:09 -0400

    début du toolbar fait (Home et favorites)

--- app/src/main/java/uqac/dim/recipetracker/MainActivity.java
+++ app/src/main/java/uqac/dim/recipetracker/MainActivity.java
@@ -1,8 +1,17 @@
 package uqac.dim.recipetracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //crée la bar de menu en bas de l'écran
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_toolbar);
+        bottomNav.setOnItemSelectedListener(navListener);
+
+        //ouverture de l'appli avec l'écran home
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
+    }
+
+    private BottomNavigationView.OnItemSelectedListener navListener =
+            new BottomNavigationView.OnItemSelectedListener(){
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item){
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()){
+                        case R.id.home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.favorites:
+                            selectedFragment = new FavoritesFragment();
+                            break;
+                    }
+
+                    assert selectedFragment != null;
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+                    return true;
+                }
+            };
+
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    public void ouvrirRecette(View v){
+        Log.i("DIM","Poutine");
     }
 }
